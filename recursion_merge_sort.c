be2dd1803385407d9199fc9a64da7ca90d6801e6
TomasNagy <tomi.nagy@outlook.com>
2023-06-01 20:21:01 +0200

    feat: merge sorting algorithm

--- recursion_merge_sort.c
+++ recursion_merge_sort.c
@@ -2,7 +2,107 @@
 
 #define ARRAY_LENGTH 11
 
+void copy_array(int from[], int to[], int start, int end);
+void merge_sort(int unsorted[], int sorted[], int start_index, int end_index);
+void merge_left_right(int left[], int right[], int start_index, int mid_index, int end_index);
+void print_array(int array[], int start, int end);
+
 int main(void)
 {
-    int unsorted_numbers[ARRAY_LENGTH] = {10, 5, 7, 4, 2, 1, 0, 8, 6, 9, 3};
+    int unsorted_numbers[ARRAY_LENGTH] = {5, 7, 6, 3, 1, 9, 0, 2, 10, 4, 8}; 
+
+    int sorted_numbers[ARRAY_LENGTH];
+
+    copy_array(unsorted_numbers, sorted_numbers, 0, ARRAY_LENGTH - 1);
+
+    print_array(unsorted_numbers, 0, ARRAY_LENGTH - 1);
+
+    merge_sort(unsorted_numbers, sorted_numbers, 0, ARRAY_LENGTH - 1);
+
+    return 0;
+}
+
+void copy_array(int from[], int to[], int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        to[i] = from[i]; 
+    }
+
+    return;
+}
+
+void merge_sort(int unsorted[], int sorted[], int start_index, int end_index)
+{
+    // Define base-case / exit condition
+    // Consider list already sorted
+    if (end_index - start_index < 1)
+    {
+        return;
+    }
+
+    int mid = (start_index + end_index) / 2;
+
+    // Sort left
+    // print_array(unsorted, start_index, mid);
+    merge_sort(unsorted, sorted, start_index, mid);
+
+    // Sort rigth
+    // print_array(unsorted, mid + 1, end_index);
+    merge_sort(unsorted, sorted, mid + 1, end_index);
+
+    // Merge left and right
+    merge_left_right(unsorted, sorted, start_index, mid + 1, end_index);
+
+    return;
+}
+
+void print_array(int array[], int start, int end)
+{
+    printf ("[");
+
+    for (int i = start; i <= end; i++)
+    {
+        printf ("%i ", array[i]);
+    }
+    printf ("]\n");
+
+    return;
+}
+
+void merge_left_right(int unsorted[], int sorted[], int start_index, int mid_index, int end_index)
+{
+    int mid = mid_index;
+    int start = start_index;
+
+    for (int i = start; i <= end_index; i++)
+    {
+        if (mid > end_index)
+        {
+            mid = start;
+        }
+
+        if (start >= mid_index)
+        {
+            start = mid; 
+        }
+
+        if (unsorted[start] < unsorted[mid])
+        {
+            sorted[i] = unsorted[start];
+            start++;
+        }
+        else
+        {
+            sorted[i] = unsorted[mid];
+            mid++;
+        }
+    }
+
+    copy_array(sorted, unsorted, start_index, end_index);
+
+    printf ("Sorted:\n");
+    print_array(unsorted, start_index, end_index);
+
+    return;
 }
